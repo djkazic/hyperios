145b0c70060b0762331a89f74cae81c0d73c3d11
Kevin Cai <kevincai96@gmail.com>
2016-04-02 17:05:31 -0400

    Static refactor of Google places API invoke

--- src/main/java/org/alopex/hyperios/Core.java
+++ src/main/java/org/alopex/hyperios/Core.java
@@ -1,44 +1,25 @@
 package org.alopex.hyperios;
 
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import org.bson.Document;
-
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Place;
+import org.alopex.hyperios.util.GeoUtils;
 
 public class Core {
+	
 	public static void main(String[] args) {
-		System.out.println("Connecting to Google Places API...");
-		GooglePlaces client = new GooglePlaces(Settings.placeApiKey);
+		System.out.println("Silencing MongoDB debug output...\n");
+		suppressMongoDB();
 		
-		System.out.println("Polling hospitals...");
-		List<Place> places = client.getPlacesByQuery("Boston hospitals", 40);
+		GeoUtils.doSearch("Hospitals in Boston", "medical");
+		GeoUtils.doSearch("Subways in Boston", "transportation");
 		
-		System.out.println("Found " + places.size() + " results.");
-		
-		System.out.println("Listing locations:");
-		for (Place place : places) {
-			System.out.println("\t" + place.getName() + " location: " + place.getAddress());
-			Document poiDoc = new Document("type", "medical")
-								   .append("address", place.getAddress())
-								   .append("density", "-1");
-			DB.getDatabase().getCollection("poi").insertOne(poiDoc);
-		}
-		
-		//TODO: remake client object
-		
-		client = new GooglePlaces(Settings.placeApiKey);
-		
-		System.out.println("Searching for transportation infrastructure...");
-		List<Place> placesTransport = client.getPlacesByQuery("Boston subway", 40);
-		
-		System.out.println("Found " + placesTransport.size() + " results.");
-		
-		System.out.println("Listing locations:");
-		for (Place place : placesTransport) {
-			//Place detailPlace = place.getDetails();
-			System.out.println("\t" + place.getName() + " location: " + place.getAddress());
-		}
+		System.out.println();		
+	}
+	
+	private static void suppressMongoDB() {
+		System.setProperty("DEBUG.MONGO", "false");          
+		System.setProperty("DB.TRACE", "false");             
+		Logger.getLogger("com.mongodb").setLevel(Level.OFF);
 	}
 }
---
+++ src/main/java/org/alopex/hyperios/util/GeoUtils.java
@@ -0,0 +1,34 @@
+package org.alopex.hyperios.util;
+
+import java.util.List;
+
+import org.alopex.hyperios.DB;
+import org.alopex.hyperios.Settings;
+import org.bson.Document;
+
+import se.walkercrou.places.GooglePlaces;
+import se.walkercrou.places.Place;
+
+public class GeoUtils {
+	
+	private static GooglePlaces client;
+	
+	public static void doSearch(String query, String type) {
+		client = new GooglePlaces(Settings.placeApiKey);
+		
+		System.out.println("Searching for [" + query + "]...");
+		List<Place> placesTransport = client.getPlacesByQuery(query, 40);
+		System.out.println("Found " + placesTransport.size() + " results.");
+		System.out.println("Listing locations:");
+		for (Place place : placesTransport) {
+			String address = place.getAddress();
+			if (!address.equals("United States")) {
+				System.out.println("\t" + place.getName() + " location: " + address);
+				Document poiDoc = new Document("type", type)
+						   .append("address", address)
+						   .append("density", "-1");
+				DB.getDatabase().getCollection("poi").insertOne(poiDoc);
+			}
+		}
+	}
+}
