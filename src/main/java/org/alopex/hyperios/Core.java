1545ba48902113bb22976265eed1f3a0510253e1
Kevin Cai <kevincai96@gmail.com>
2016-04-02 17:25:27 -0400

    Implemented prelim API endpoint

--- src/main/java/org/alopex/hyperios/Core.java
+++ src/main/java/org/alopex/hyperios/Core.java
@@ -3,6 +3,7 @@ package org.alopex.hyperios;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.alopex.hyperios.net.api.APIRouter;
 import org.alopex.hyperios.util.GeoUtils;
 
 public class Core {
@@ -11,6 +12,9 @@ public class Core {
 	
 	//TODO: make an invoke method with City var
 	public static void main(String[] args) {
+		System.out.println("Initializing API endpoint...\n");
+		APIRouter.init();
+		
 		System.out.println("Silencing MongoDB debug output...\n");
 		suppressMongoDB();
 		
@@ -19,8 +23,8 @@ public class Core {
 		
 		gutils.doSearch("Hospitals in Boston", "medical");
 		System.out.println();
-		
-		gutils.doSearch("Subways in Boston", "transportation");		
+		gutils.doSearch("Subway stops in Boston", "transportation");
+		System.out.println();
 	}
 	
 	private static void suppressMongoDB() {
---
+++ src/main/java/org/alopex/hyperios/net/api/APIRouter.java
@@ -0,0 +1,44 @@
+package org.alopex.hyperios.net.api;
+
+import java.util.logging.LogManager;
+
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.Restlet;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+import org.restlet.routing.Router;
+
+public class APIRouter extends Application {
+
+	public static void init() {
+		try {
+			Component component = new Component();
+			component.getServers().add(Protocol.HTTP, 8888);
+			component.getDefaultHost().attach(new APIRouter());
+			component.start();
+			LogManager.getLogManager().reset();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Registers /api/ restlet for bridge routing
+	 */
+	public Restlet createInboundRoot() {
+		Router router = new Router(getContext());
+		router.attachDefault(EchoPage.class);
+		// router.attach("/api/sample/count", SampleCount.class);
+		return router;
+	}
+	
+	private class EchoPage extends ServerResource {
+		
+		@Get
+		public String process() {
+			return "Hyperios API v1.0";
+		}
+	}
+}
