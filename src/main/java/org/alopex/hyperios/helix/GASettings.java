3719051443ab931d97ac4c13409eaf2fbc51f133
Kevin Cai <kevincai96@gmail.com>
2016-04-03 11:01:30 -0400

    Fixed preposterous tournament ratio

--- src/main/java/org/alopex/hyperios/helix/GASettings.java
+++ src/main/java/org/alopex/hyperios/helix/GASettings.java
@@ -3,7 +3,7 @@ package org.alopex.hyperios.helix;
 public class GASettings {
 
 	public static int populationSize     = 35;
-	public static double tournamentRatio = 0.2;
+	public static double tournamentRatio = 0.1;
 	public static int genomeSize         = 16;
 	public static double crossoverRate   = 0.5;
 	public static double mutateRate      = 0.0125;
